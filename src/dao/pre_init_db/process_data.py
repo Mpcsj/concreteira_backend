991599e381628b327211674f26dfa012f9be0fe3
mpcsj <marcosjunioribm@gmail.com>
2020-01-17 18:57:41 -0300

    Atualizando funcoes

--- src/dao/pre_init_db/process_data.py
+++
@@ -1,6 +0,0 @@
-file_res = open('result.py','w')
-with open('insert_initial_data.py','r') as f:
-	for line in f:
-		file_res.write(line.replace('Pedreia Um Valemix','Pedreira Um Valemix'))
-
-file_res.close()
